47eba65b90040be3cb942f6c1e0161d24fedddd1
Habib <habibjutt868@gmail.com>
2020-09-28 21:01:01 +0400

    appointments

--- src/components/App.js
+++ src/components/App.js
@@ -9,21 +9,44 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      "Name": "Habib"
-    }
+      "myAppointments": [],
+      "lastIndex": 0
+    };
+  }
+
+  componentDidMount () {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then( result => {
+      const apts = result.map(item => {
+        item.aptId = this.state.lastIndex;
+        this.setState(
+          {
+            lastIndex: this.state.lastIndex + 1
+          }
+        )
+        return item;
+      });
+
+      this.setState(
+        {
+          myAppointments: apts
+        }
+      )
+    })
   }
 
   render () {
+
     return (
       <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              {this.state.Name}
               <AddAppointments />
               <SearchAppointment />
-              <ListAppointments />
+              <ListAppointments appointments = {this.state.myAppointments} />
             </div>
           </div>
         </div>
